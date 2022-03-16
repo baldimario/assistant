745b006a36005c2a838568a84fce3fbd0632289e
Mario <mario.baldi@ofcourse.me>
2022-03-16 11:47:44 +0100

    fallback exact key

--- lib/Brain.py
+++ lib/Brain.py
@@ -6,12 +6,15 @@ class Brain:
     def __init__(self, memory, language='it') -> None:
         self.m_language = language
         self.m_memory = memory
-        self.m_memory.load()
         fasttext.util.download_model(self.m_language, if_exists='ignore') 
         self.m_ft = fasttext.load_model('cc.it.300.bin')
+        self.init()
+        print('Ready.')
+    
+    def init(self):
+        self.m_memory.load()
         self.m_hooks = self.m_memory.get_hooks()
         self.m_hooks_vec = [self.sentence2vec(hook) for hook in self.m_hooks if hook is not '_']
-        print('Ready.')
 
     def sentence2vec(self, sentence):
         words = [self.m_ft[word] for word in sentence.split(' ') if word in self.m_ft.words]
@@ -26,11 +29,15 @@ class Brain:
             return self.m_hooks[min_idx]
 
     def think(self, phrase):
+        self.init()
         phrase_vec = self.sentence2vec(phrase)
         response_hook = self.recall(phrase_vec)
         if response_hook in self.m_hooks:
             response_cmd = self.m_memory.get(response_hook)
         else:
-            response_cmd = self.m_memory.get('_')
+            if phrase in self.m_hooks:
+                response_cmd = self.m_memory.get(phrase)
+            else:
+                response_cmd = self.m_memory.get('_')
         output = subprocess.check_output(['sh', '-c', response_cmd]).decode('utf-8')
         return output
