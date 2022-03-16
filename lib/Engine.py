9be4d93600e493cd76ae8add21ac0c44938fec99
Mario <mario.baldi@ofcourse.me>
2022-03-16 10:54:21 +0100

    assistente is the trigger word

--- lib/Engine.py
+++ lib/Engine.py
@@ -7,7 +7,16 @@ class Engine:
         self.m_brain = self.m_dependencies['brain'](self.m_memory)   
 
     def run(self):
+        state = False
         for phrase in self.m_ears.listen():
-            self.m_voice.play('beep.mp3')
-            sentence = self.m_brain.think(phrase)
-            self.m_voice.speak(sentence)
+            words = phrase.split(' ')
+            if words[0] == 'assistente':
+                self.m_voice.play('beep.mp3')
+                words = words[1:]
+                state = True
+            
+            if state == True and len(words) > 1:
+                phrase = ' '.join(words)
+                sentence = self.m_brain.think(phrase)
+                self.m_voice.speak(sentence)
+                state = False
